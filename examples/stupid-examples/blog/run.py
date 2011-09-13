c27228cd6f284cd75978db9068b79f1518c5c407
Brosseau Valentin <c4software@gmail.com>
2011-09-13 23:01:21 +0200

    Ajout d'exemple stupid du blog

---
+++ examples/stupid-examples/blog/run.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from pyIvr.ext.flask.baseFlask import baseFlask
+from pyIvr.decorator import render
+from pyIvr.dynamiqueIvr import dynamiqueIvr
+
+app = Flask(__name__)
+
+blog = dynamiqueIvr("blog.json")
+
+@app.route("/")
+@app.route("/<step>")
+@render('xhtml','1.0')
+def home(step=None):
+  if step is None:
+    return blog.get()
+  else:
+    return {"params":blog.getParams(),"svi":blog.getStep(step,True,"/")}
+	
+app.run(host='0.0.0.0', debug=True)
