1a21c7e8bf7c06621c3c65517c0a024f1c252ee3
Brosseau Valentin <c4software@gmail.com>
2011-09-19 21:56:33 +0200

    oups suppression d'un import ivrBlueprint

--- examples/flask-based-examples/flask_blueprint_dynamique.py
+++ examples/flask-based-examples/flask_blueprint_dynamique.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from pyIvr.ext.flask.blueprintFlaskIvr import ivrBlueprint, ivrBlueprintUri
+from pyIvr.ext.flask.blueprintFlaskIvr import ivrBlueprint
 
 app = Flask(__name__)
 app.register_blueprint(ivrBlueprint)
