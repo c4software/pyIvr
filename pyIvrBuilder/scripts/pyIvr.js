9f839ab5fcdc66a497ae590f63156cf10bb7746a
Brosseau Valentin <c4software@gmail.com>
2011-10-15 18:33:04 +0200

    Test d'interface

--- pyIvrBuilder/scripts/pyIvr.js
+++ pyIvrBuilder/scripts/pyIvr.js
@@ -1,14 +1,15 @@
 ;(function() {
-	
+	windows = [],
+
 	window.jsPlumbDemo = {
 		init : function() {
 				
 			// default drag options
 			jsPlumb.Defaults.DragOptions = { cursor: 'pointer', zIndex:2000 };
 			// default to blue at one end and green at the other
-			jsPlumb.Defaults.EndpointStyles = [{ fillStyle:'#225588' }, { fillStyle:'#558822' }];
+			jsPlumb.Defaults.EndpointStyles = [{ fillStyle:'#FF0000' }, { fillStyle:'#558822' }];
 			// blue endpoints 7 px; green endpoints 11.
-			jsPlumb.Defaults.Endpoints = [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]];
+			jsPlumb.Defaults.Endpoints = [ [ "Dot", {radius:7} ], [ "Dot", { radius:7 } ]];
 			// enable mouse events
 			jsPlumb.setMouseEventsEnabled(true);						
 			// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
@@ -32,32 +33,25 @@
 			},
 			// .. and this is the hover style. 
 			connectorHoverStyle = {
-				lineWidth:7,
+				lineWidth:5,
 				strokeStyle:"#2e2aF8"
 			},
-			// the definition of source endpoints (the small blue ones)
-			sourceEndpoint = {
-				endpoint:"Dot",
-				paintStyle:{ fillStyle:"#225588",radius:7 },
-				isSource:true,
-				connector:[ "Flowchart", { stub:40 } ],
-				connectorStyle:connectorPaintStyle,
-				hoverPaintStyle:connectorHoverStyle,
-				connectorHoverStyle:connectorHoverStyle
-			},
-			// a source endpoint that sits at BottomCenter
-			bottomSource = jsPlumb.extend( { anchor:"BottomCenter" }, sourceEndpoint),
+
+
+			sourceEndpoint = {  isSource:true, 
+								endpoint:["Dot", {radius:7 }],
+								connector:[ "Flowchart", { stub:40 } ],
+								anchor:"BottomCenter",
+								hoverPaintStyle:connectorHoverStyle,
+								connectorStyle:connectorPaintStyle,
+							 },
+							 
 			// the definition of target endpoints (will appear when the user drags a connection) 
-			targetEndpoint = {
-				endpoint:"Dot",					
-				paintStyle:{ fillStyle:"#558822",radius:11 },
-				hoverPaintStyle:connectorHoverStyle,
-				maxConnections:-1,
-				dropOptions:{ hoverClass:"hover", activeClass:"active" },
-				isTarget:true,
-				anchor:[ "LeftMiddle", "RightMiddle" ]
-			},
-			windows = ["window1", "window2", "window3", "window4"],
+			targetEndpoint = { 
+								isTarget:true, 
+								endpoint:[ "Dot", { radius:7 } ]
+							 },
+
 			init = function(connection) {
 				connection.labelText = connection.sourceId.substring(6) + "-" + connection.targetId.substring(6);
 			};
@@ -66,36 +60,14 @@
 			// add endpoints to all windows. note here we use a string array; that's just because this demo is framework-agnostic.  you'd
 			// probably use a selector in the real world, eg.
 			//
-			// jsPlumb.addEndpoint($(".window"), [ bottomSource ]);
-			//
-			var sourceEndpoints = jsPlumb.addEndpoint(windows, bottomSource),
-			targetEndpoints = jsPlumb.addEndpoint(windows, targetEndpoint); 
-			
+			jsPlumb.addEndpoint($(".window"),sourceEndpoint);
+			jsPlumb.addEndpoint($(".window"),targetEndpoint);
+						
 			// listen for new connections; initialise them the same way we initialise the connections at startup.
 			jsPlumb.bind("jsPlumbConnection", function(connInfo) { 
 				init(connInfo.connection);
 			});
-								
-			//
-			// make all windows drop targets.  again note the string array vs selector issue.
-			//
-			/*jsPlumb.makeTarget(windows, {
-				endpoint:targetEndpoint,
-				dropOptions:{ hoverClass:"hover", activeClass:"active" },
-				deleteEndpointsOnDetach:true
-			});*/
-			
-			// make a couple of connections. note that the return value of addEndpoints is an array of Endpoints, 
-			jsPlumb.connect({
-				source:sourceEndpoints[0],
-				target:targetEndpoints[1]
-			});
-			
-			jsPlumb.connect({
-				source:sourceEndpoints[3],
-				target:targetEndpoints[2]
-			});		
-
+										
 			//
 			// listen for clicks on connections, and offer to delete connections on click.
 			//
